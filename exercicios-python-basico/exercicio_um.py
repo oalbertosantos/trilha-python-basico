a335f93b5091cdd969f7e8e850cc7e4925d6874c
Alberto Santos <alberto.almcz@outlook.com>
2024-01-21 00:53:17 -0300

    Update exercicio_um.py

--- exercicios-python-basico/exercicio_um.py
+++ exercicios-python-basico/exercicio_um.py
@@ -1,4 +1,4 @@
-#Converta as seguintes expressões matemáticas para que possam ser calculadas usando o interpretador Python: a) 10 + 20 x 30, b) 4²/30, c) (9**4 +2) x 6-1.
+# 1 - Converta as seguintes expressões matemáticas para que possam ser calculadas usando o interpretador Python: a) 10 + 20 x 30, b) 4²/30, c) (9**4 +2) x 6-1.
 
 # a)
 
@@ -32,4 +32,15 @@ operacao = (f * g)
 resultado = operacao
 
 print(f"O resultado é: {resultado:.2f}")
+print()
+
+# 2 - Digite a seguinte expressão no interpretador: 10 % 3 x 10 ** 2 + 1 - 10 x 4/2.
+
+h = (10 % 3 * 10 ** 2 + 1)
+i = (10 * 4/2)
+
+operacao = (h - i)
+resultado = operacao
+
+print("O resultado é: ", resultado)
 print()
